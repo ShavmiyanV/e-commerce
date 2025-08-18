e3062e38ca77b8d1cd03ec2ef81f238fba71cdc6
Shavmiyan <shavmiyan16@gmail.com>
2025-08-19 03:18:47 +0530

    Add auto-slide and navigation to Slider component
    Introduced automatic slide transition using useEffect and setInterval, and added navigation dots for manual slide selection. Improved layout and styling for better responsiveness and user experience.

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -33,16 +33,28 @@ const slides = [
 
 const Slider = () => {
   const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="h-[calc(100vh-80px)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000">
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
+      >
         {slides.map((slide) => (
           <div
             className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
             key={slide.id}
           >
             {/* TEXT CONTAINER */}
-            <div className="h-1/2 xl:w-1/2">
+            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
               <h2 className="text-xl lg:text-3xl 2xl:text-5xl ">
                 {slide.description}
               </h2>
@@ -56,7 +68,7 @@ const Slider = () => {
               </Link>
             </div>
             {/* IMAGE CONTAINER */}
-            <div className="h-1/2 xl:w-1/2 relative">
+            <div className="h-1/2 xl:w-1/2 relative xl:h-full">
               <Image
                 src={slide.img}
                 alt=""
@@ -65,6 +77,21 @@ const Slider = () => {
                 className="object-cover"
               />
             </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+        {slides.map((slide, index) => (
+          <div
+            className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150" : ""
+            }`}
+            key={slide.id}
+            onClick={() => setCurrent(index)}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
           </div>
         ))}
       </div>
