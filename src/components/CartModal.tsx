5098c7068f823ad8f227e7a181aba43ee758da8e
Shavmiyan <shavmiyan16@gmail.com>
2025-08-19 01:36:59 +0530

    Redesign cart modal layout and content
    Enhanced the CartModal component with a new layout, including a shopping cart title, itemized product list, subtotal section, and action buttons for viewing cart and checkout. Improved styling and structure for better user experience.

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -6,32 +6,88 @@ const CartModal = () => {
   const cartItems = true;
 
   return (
-    <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
       {!cartItems ? (
         <div className="">Cart is Empty</div>
       ) : (
-        <div className="">
-          <Image
-            src="https://images.pexels.com/photos/33269728/pexels-photo-33269728.jpeg"
-            alt=""
-            width={72}
-            height={96}
-            className="object-cover rounded-md"
-          />
-          <div className="">
-            {/* TOP */}
-            <div className="">
-              {/* TITLE */}
-              <div className="">
-                <h3>Product Name</h3>
-                <div>$49</div>
+        <>
+        <h2 className="text-xl">Shopping Cart</h2>
+          {/* LIST */}
+          <div className="flex flex-col gap-8">
+            {/* ITEM */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/33269728/pexels-photo-33269728.jpeg"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div className="">
+                  {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-100 rounded-sm">$49</div>
+                  </div>
+                  {/* DESCRIPTION */}
+                  <div className="text-sm text-gray-500">avilable</div>
+                </div>
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
               </div>
-              {/* DESCRIPTION */}
-              <div className="">avilable</div>
             </div>
-            {/* BOTTOM */}
+            {/* ITEM */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/33269728/pexels-photo-33269728.jpeg"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div className="">
+                  {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-100 rounded-sm">$49</div>
+                  </div>
+                  {/* DESCRIPTION */}
+                  <div className="text-sm text-gray-500">avilable</div>
+                </div>
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+          {/* BOTTOM */}
+          <div className="">
+            <div className="flex items-center justify-between font-semibold">
+              <span className="">Subtotal</span>
+              <span className="">$49</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4">
+              Shipping and taxes calculated at checkout.
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300 ">
+                View Cart
+              </button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
