fc9e0c19636026e41065a2bc16f4c0d36f58f997
Shavmiyan <shavmiyan16@gmail.com>
2025-08-19 01:08:20 +0530

    Enhance cart modal and navbar UI, update configs
    Improved CartModal to display product details and image, added cart item count badge in NavIcons, and expanded Navbar links for larger screens. Updated next.config.mjs to allow remote images from Pexels and added custom 'lama' color to Tailwind config.

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,9 +1,40 @@
-"use client"
+"use client";
+
+import Image from "next/image";
 
 const CartModal = () => {
-  return (
-    <div>CartModal</div>
-  )
-}
+  const cartItems = true;
 
-export default CartModal
+  return (
+    <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+      {!cartItems ? (
+        <div className="">Cart is Empty</div>
+      ) : (
+        <div className="">
+          <Image
+            src="https://images.pexels.com/photos/33269728/pexels-photo-33269728.jpeg"
+            alt=""
+            width={72}
+            height={96}
+            className="object-cover rounded-md"
+          />
+          <div className="">
+            {/* TOP */}
+            <div className="">
+              {/* TITLE */}
+              <div className="">
+                <h3>Product Name</h3>
+                <div>$49</div>
+              </div>
+              {/* DESCRIPTION */}
+              <div className="">avilable</div>
+            </div>
+            {/* BOTTOM */}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CartModal;
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -45,14 +45,18 @@ const NavIcons = () => {
         height={22}
         className="cursor-pointer"
       />
-      <Image
-        src="/cart.png"
-        alt=""
-        width={22}
-        height={22}
-        className="cursor-pointer"
-        onClick={() => setIsCartOpen((prev) => !prev)}
-      />
+      <div className="relative cursor-pointer">
+        <Image
+          src="/cart.png"
+          alt=""
+          width={22}
+          height={22}
+          onClick={() => setIsCartOpen((prev) => !prev)}
+        />
+        <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center">
+          2
+        </div>
+      </div>
       {isCartOpen && <CartModal />}
     </div>
   );
