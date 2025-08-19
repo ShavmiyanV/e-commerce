edb738e567946f571e402a9a012bf5a91665ecda
Shavmiyan <shavmiyan16@gmail.com>
2025-08-20 03:33:33 +0530

    Implement styled Footer component with contact info
    Replaces the placeholder Footer with a styled layout including contact details, social media icons, and a subscription section. Uses Next.js Link and Image components for navigation and image rendering.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-gray-200 text-sm mt-24">
+      {/* TOP */}
+      <div className="flex justify-between gap-24">
+        {/* LEFT */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <Link href="/">
+            <div className="text-2xl tracking-wide">LAMA</div>
+          </Link>
+          <p>Galle Road, Colombo-6, Sri Lanka.</p>
+          <span className="font-semibold">shavmiyan16@gmail.com</span>
+          <span className="font-semibold">+94768816747</span>
+          <div className="flex gap-6">
+            <Image src="/facebook.png" alt="" width={16} height={16} />
+            <Image src="/instagram.png" alt="" width={16} height={16} />
+            <Image src="/youtube.png" alt="" width={16} height={16} />
+            <Image src="/pinterest.png" alt="" width={16} height={16} />
+            <Image src="/x.png" alt="" width={16} height={16} />
+          </div>
+        </div>
+        {/* CENTER */}
+        <div className="hidden lg:flex justify-between w-1/2"></div>
+        {/* RIGHT */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p className="">
+            Be the first to get the latest news about trends, promotions, and
+            much more!
+          </p>
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Email Address"
+              className="p-4 w-3/4"
+            />
+          </div>
+        </div>
+      </div>
+      {/* BOTTOM */}
+      <div className=""></div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
