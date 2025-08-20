d7ac793234c34c7c6a80b9c38407ee34eb200894
Shavmiyan <shavmiyan16@gmail.com>
2025-08-21 02:55:21 +0530

    Update Add component UI and structure
    Enhanced the Add component with a quantity selection header, styled container, and improved Add to Cart button. Refactored markup for better layout and user experience.

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,9 +1,13 @@
-
-
 const Add = () => {
   return (
-    <div>Add</div>
-  )
-}
+    <div className="flex flex-col gap-4">
+      <h4 className="font-medium">Choose a Quantity</h4>
+      <div className=""></div>
+      <button className="w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
+        Add to Cart
+      </button>
+    </div>
+  );
+};
 
-export default Add
+export default Add;
